a2432e84c2e1065e60cb77ed15779ceda842b845
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-11-27 02:16:41 -0300

    stable version

--- src/main/java/io/harness/payments/api/PaymentValidation.java
+++ src/main/java/io/harness/payments/api/PaymentValidation.java
@@ -14,6 +14,7 @@ public abstract class PaymentValidation {
         }
         try {
             Thread.sleep(5000);
+
             invoice.setStatus("verified");
             payments.add(invoice);
             return invoice;
