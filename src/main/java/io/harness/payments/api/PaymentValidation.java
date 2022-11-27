c544b939301112aa5333711eeebee490030caa24
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-11-27 14:20:38 -0300

    stable version

--- src/main/java/io/harness/payments/api/PaymentValidation.java
+++ src/main/java/io/harness/payments/api/PaymentValidation.java
@@ -1,20 +1,37 @@
 package io.harness.payments.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.ws.rs.core.Response;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.SecureRandom;
 import java.util.*;
 
-
+@Slf4j
 public abstract class PaymentValidation {
     List payments =  new ArrayList();
 
+    private SecureRandom r = new SecureRandom();
 
     public Payment validate(Payment invoice){
+        int max = 800, min = 1000;
+
+        int msDelay = r.nextInt((max - min) + 1) + min;
+
         if (payments.size() >= 1000){
             this.payments = payments.subList(100,payments.size()-1);
         }
         try {
-            Thread.sleep(3000);
+            Thread.sleep(msDelay);
+
+            if (r.nextInt((100 - 1) + 1) < 3) {
+
+                log.error("ERROR [Payment Validation] - Failed to validate invoice");
+                invoice.setStatus("failed-bug");
+            }
+
             invoice.setStatus("verified");
             payments.add(invoice);
             return invoice;
@@ -32,6 +49,26 @@ public abstract class PaymentValidation {
     @JsonProperty
     public List getPayments() {
         return payments;
+    }
+
+    private String getVersion(){
+        String version = "stable";
+        try {
+            InetAddress inetAdd = InetAddress.getLocalHost();
+
+            String name = inetAdd.getHostName();
+
+            log.debug("Diego - hostname: "+name);
+
+            if(name.contains("canary")){
+                log.debug("Diego - Set as Canary");
+                version = "canary";
+            }
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+
+        return version;
     }
 
 
