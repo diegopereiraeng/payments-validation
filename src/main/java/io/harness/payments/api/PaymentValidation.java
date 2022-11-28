bb283368640fe5c6acd6fe30272ee3b6fd12410d
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-11-28 16:24:04 -0300

    bug demo

--- src/main/java/io/harness/payments/api/PaymentValidation.java
+++ src/main/java/io/harness/payments/api/PaymentValidation.java
@@ -65,7 +65,7 @@ public abstract class PaymentValidation {
 
 
     public Payment validate(Payment invoice){
-        int max = 4000, min = 3900;
+        int max = 4000, min = 3000;
 
         log.debug("starting payment validation");
 
