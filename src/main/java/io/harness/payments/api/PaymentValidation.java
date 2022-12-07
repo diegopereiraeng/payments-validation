50746982df9fb22ce6667b7f5344467aa0e713ea
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-12-07 10:44:24 -0300

    auto load

--- src/main/java/io/harness/payments/api/PaymentValidation.java
+++ src/main/java/io/harness/payments/api/PaymentValidation.java
@@ -161,7 +161,7 @@ public abstract class PaymentValidation {
             // Comment this for you stable version or first deployment
             // Set here the increased response time with ff Experiment enabled
             // change "canary" to "not-bug" and vice versa to enable canary bug or not
-            if (this.betaFeature && getVersion() == "not-bug") {
+            if (this.betaFeature && getVersion() == "canary") {
                 max = 5000;
                 min = 4900;
                 errorPercentage = 95;
