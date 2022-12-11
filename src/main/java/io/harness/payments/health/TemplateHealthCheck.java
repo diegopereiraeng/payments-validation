d2328321de86b9930c51f6d0e2b5cfc9f0ef09b9
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-12-10 22:10:08 -0300

    mem fix

--- src/main/java/io/harness/payments/health/TemplateHealthCheck.java
+++ src/main/java/io/harness/payments/health/TemplateHealthCheck.java
@@ -1,7 +1,9 @@
 package io.harness.payments.health;
 
 import com.codahale.metrics.health.HealthCheck;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TemplateHealthCheck extends HealthCheck {
     private final String template;
 
@@ -13,6 +15,7 @@ public class TemplateHealthCheck extends HealthCheck {
     protected Result check() throws Exception {
         final String saying = String.format(template, "TEST");
         if (!saying.contains("TEST")) {
+            log.error("Health Check Failed");
             return Result.unhealthy("template doesn't include a name");
         }
         return Result.healthy();
